2dd0e02f9de122c95d18c7f8d53b7341834e408d
Nataliya Bernatska <62563765+Natali-Bern@users.noreply.github.com>
2021-10-26 17:52:47 +0300

    update pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,20 +1,61 @@
 import './Pagination.css';
+// import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Pages from '../Pages';
+import { TO_NEXT, TO_PREV } from '../../actions/paginationActions';
 
 function Pagination() {
+
+    const pages = useSelector(state => state.pagination);
+    const dispatch = useDispatch();
+
+    const pagesArray = [];
+
+//   for (let i = 0; i < pages.totalPage; i++) {
+//     pagesArray.push(i + 1);
+//   }
+
+  function toNextHandler() {
+    dispatch({
+      type: TO_NEXT
+    })
+  }
+
+  function toPrevHandler() {
+    dispatch({
+      type: TO_PREV
+    })
+  }
+
+
+
+
+
+
+
+
     return (
         
         <div className="pagination">
-                <a className="pagination__link pagination__prev" href="#">&laquo;</a>
+                <a className="pagination__link pagination__prev" href="#" onClick={toPrevHandler}>&laquo;</a>
 
                 <div className="pagination__group">
+                {pagesArray.map((item, index) =>
+
+                    <Pages
+                        page={item}
+                        key={index}
+                        active={pages.currentPage == item ? 'active' : ""}
+                    />
+                )}
                     <a className="pagination__link" href="#">1</a>
                     <a className="pagination__link pagination__link_active" href="#">2</a>
                     <a className="pagination__link" href="#">3</a>
                     <a className="pagination__link" href="#">4</a>
                     <a className="pagination__link" href="#">5</a>
-                    <a className="pagination__link" href="#">6</a>
+                    <a className="pagination__link" href="#">6</a> 
                 </div>
-                <a className="pagination__link pagination__next" href="#">&raquo;</a>
+                <a className="pagination__link pagination__next" href="#" onClick={toNextHandler}>&raquo;</a>
             </div> 
         
     );
